0679a2419e7d4ba5a20ccf63fc1e44fe7117192c
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-23 00:16:37 -0300

    fix: test stk ai script

--- application.py
+++ application.py
@@ -1,1 +1,15 @@
-print("Hello World!!!")
+import subprocess
+
+stk_ia = "stk ai"
+
+question = "Como posso fazer um bom code review?"
+
+process = subprocess.Popen(stk_ia.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+stdout, stderr = process.communicate(question)
+
+# Verifica se houve algum erro
+if process.returncode != 0:
+    print(f"Ocorreu um erro: {stderr}")
+else:
+    print(f"Saída do comando: {stdout}")
