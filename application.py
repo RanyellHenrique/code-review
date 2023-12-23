77cbd3ae3713eccdcd2734e750a8ed7e3eaf1d1b
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-23 13:02:33 -0300

    fix: test

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ print("Enviando a pergunta para o stk ai")
 child.sendline(question)
 
 print("Precionando esc + enter")
-child.sendline('\x1b\r')
+child.sendline("\x1b\x40\r")
 
 child.expect('otsop')
 
