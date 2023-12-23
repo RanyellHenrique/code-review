6b7d776874455b399182e372b6b6dc0dea06f416
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-23 11:52:50 -0300

    fix: edit script

--- application.py
+++ application.py
@@ -18,9 +18,9 @@ child.send('\x1b\x0d')
 child.expect('>>>')
 
 # Obtém a saída do processo
-before = child.before.decode('utf-8')
-after = child.after.decode('utf-8')
-buffer = child.buffer.decode('utf-8')
+before = child.before
+after = child.after
+buffer = child.buffer
 
 # Imprime a saída
 print(f"Saída do comando(before): {before}")
