6e6edd194e6b917112b1f5b90565aadebfe4b497
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-23 01:35:37 -0300

    fix: edit expect

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ question = "Como posso fazer um bom code review?"
 
 child = pexpect.spawn(stk_ia)
 
-child.expect('(Envie a requisi')
+child.expect('>>>   (Envie a requisi')
 
 # Envia a pergunta para o processo
 child.sendline(question)
@@ -18,7 +18,7 @@ child.sendcontrol('[')
 child.sendline('')
 
 # Espera a resposta
-child.expect('(Envie a requisi')
+child.expect('>>>   (Envie a requisi')
 
 # Obtém a saída do processo
 stdout = child.before
