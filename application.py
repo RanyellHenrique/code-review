682d24cabeda930337be8f0195f4eddc948c8a6c
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-23 01:34:09 -0300

    fix: add esc and enter

--- application.py
+++ application.py
@@ -7,13 +7,18 @@ question = "Como posso fazer um bom code review?"
 
 child = pexpect.spawn(stk_ia)
 
-child.expect('(Envie a requisição com ')
+child.expect('(Envie a requisi')
 
 # Envia a pergunta para o processo
 child.sendline(question)
+# Pressione Esc
+child.sendcontrol('[')
+
+# Pressione Enter
+child.sendline('')
 
 # Espera a resposta
-child.expect('(Envie a requisição com ')
+child.expect('(Envie a requisi')
 
 # Obtém a saída do processo
 stdout = child.before
