ed3d6da99aab05cf22d267e8b51a0a7c027b099c
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-26 20:16:42 -0300

    test

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
-set timeout 30
+set timeout 100
 spawn stk ai
 expect ">>>"
-sleep 1
+sleep 0.5
 send "Como posso fazer um bom code review? comece a resposta com INICIO ao contrario e encerre a resposta com a palavra FINAL ao Contrario \r"
-sleep 1
+sleep 0.5
 send "\x1b\x0d"
 expect "LANIF"
 interact
