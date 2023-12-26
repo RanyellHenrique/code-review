cf71713790bf38cb15458cfd5911fbe3c514070c
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-26 19:12:49 -0300

    fix: test

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/expect -f
-set timeout 100
+set timeout 30
 spawn stk ai
-expect ">>>   (Send the request with 'Alt+Enter' or 'Esc then Enter'.)"
+expect ">>>"
+sleep 2
 send "Como posso fazer um bom code review? encerre a resposta com a palavra FINAL ao Contrario\r"
 send "\033\r"
 expect "LANIF"
