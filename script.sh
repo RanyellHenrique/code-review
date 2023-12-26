6b4db0562e2b7a6eac61fd1e8b910824479df309
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-26 19:07:35 -0300

    fix: test

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 spawn stk ai
 expect ">>>"
-send -- "Como posso fazer um bom code review? encerre a resposta com a palavra FINAL ao Contrario\r"
-send -- "\033\r" # This is the ESC key followed by Enter
+send "Como posso fazer um bom code review? encerre a resposta com a palavra FINAL ao Contrario\r"
+send "\033\r"
 expect "LANIF"
 interact
