b8815ca534b73d1f363541dc9848e3ada5538d46
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-26 19:35:30 -0300

    fix: test

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 set timeout 30
 spawn stk ai
 expect ">>>   (Send the request with 'Alt+Enter' or 'Esc then Enter'.)"
-sleep 1
+sleep 4
 send "Como posso fazer um bom code review? comece a resposta com INICIO ao contrario e encerre a resposta com a palavra FINAL ao Contrario"
 send "\x1b\x0d"
-expect "LANIF"
-puts
+expect -re  `r"OICINI(.*?)LANIF"`
+echo $expect_out(1,string)
 interact
