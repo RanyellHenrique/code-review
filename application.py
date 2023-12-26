b3287a8ecf2d345cf592b81344d86721944888b6
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2023-12-26 20:46:16 -0300

    fix: test

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import subprocess
 
 mesage = 'Como posso fazer um bom code review? comece a resposta com INICIO ao contrario e encerre a resposta com a palavra FINAL ao Contrario'
 # Executar um programa que requer interação
-process = subprocess.Popen(["expect", 'script.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+process = subprocess.Popen(["expect", 'script.sh'], mesage, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 output, error = process.communicate()
 
--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/expect -f
+
+set mesage [lindex $argv 0]
+
 set timeout 100
 spawn stk ai
 expect ">>>"
 sleep 0.5
-send "Como posso fazer um bom code review? comece a resposta com INICIO ao contrario e encerre a resposta com a palavra FINAL ao Contrario \r"
+send "$mesage"
 sleep 0.5
 send "\x1b\x0d"
 expect "LANIF"
