d72b3ae054b16bc09a07bb6969cba863b7eb8349
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2024-01-11 02:09:13 -0300

    fix: cast hunk by str

--- scripts/code_review_bard.py
+++ scripts/code_review_bard.py
@@ -1,7 +1,7 @@
 from bardapi import Bard
 
 def code_review_by_hunk(hunk):
-    input_text = 'Poderia fazer o code review do diff do pull request a seguir? \n' + hunk
+    input_text = 'Poderia fazer o code review do diff do pull request a seguir? \n' + str(hunk)
     bard_output = Bard().get_answer(input_text)
     if bard_output.status_code == 200:
         return bard_output['content']
