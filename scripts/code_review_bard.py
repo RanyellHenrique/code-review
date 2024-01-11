e96b9813a99a542724b76b1d4fec19b8c401aee6
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2024-01-11 17:10:24 -0300

    fix: add prompt bard ai

--- scripts/code_review_bard.py
+++ scripts/code_review_bard.py
@@ -1,9 +1,20 @@
 from bardapi import Bard
 
+def format_input_text(hunk):
+    return f'''
+        Esta é uma solicitação pull ou parte de uma solicitação pull se a solicitação pull for muito grande.
+        Suponha que você analise este PR como um excelente engenheiro de software e um excelente engenheiro de segurança.
+        Você pode me contar os problemas com diferenças em uma solicitação pull e fornecer sugestões para melhorá-la?
+        Você pode fornecer um resumo da revisão e comentários sobre os problemas por arquivo, se algum problema importante for encontrado.
+        A resposta deve ser em português.
+        {str(hunk)}
+    '''
+
 def code_review_by_hunk(hunk):
-    input_text = 'Poderia fazer o code review do diff do pull request a seguir? \n' + str(hunk)
     try:
-        bard_output = Bard().get_answer(input_text)
+        bard_output = Bard().get_answer(format_input_text(hunk))
         return bard_output['content']
     except Exception:
         return None
+    
+
