215c971f54b5e6cd38f5d49ceac361fe48e80f2f
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2024-01-11 00:32:05 -0300

    fix: test get list comments

--- scripts/application.py
+++ scripts/application.py
@@ -1,6 +1,8 @@
 import os
 from get_diff_github import get_diff_by_path
 from post_comments_pull_request import post_comment_by_hunk
+from get_comments_pull_request import get_list_comments_pull_request
+
 
 def main():
     github_token=os.getenv("GITHUB_TOKEN")
@@ -8,18 +10,18 @@ def main():
     pull_request_number=int(os.getenv("GITHUB_PULL_REQUEST_NUMBER"))
     git_commit_hash=os.getenv("GIT_COMMIT_HASH")
 
-    diff_files = get_diff_by_path(github_token, github_repository, pull_request_number)
-    for file in diff_files:
-        for hunk in file:
-            # code_review_response = code_review_by_hunk(hunk)
-            post_comment_by_hunk(github_token, 
-                             github_repository, 
-                             pull_request_number, 
-                             git_commit_hash, 
-                             "code_review_response", 
-                             file.path, 
-                             int(hunk.target_start))
-
+    get_list_comments_pull_request(github_token, github_repository, pull_request_number)
+    #diff_files = get_diff_by_path(github_token, github_repository, pull_request_number)
+    #for file in diff_files:
+    #    for hunk in file:
+    #        # code_review_response = code_review_by_hunk(hunk)
+    #        post_comment_by_hunk(github_token, 
+    #                         github_repository, 
+    #                         pull_request_number, 
+    #                         git_commit_hash, 
+    #                         "code_review_response", 
+    #                         file.path, 
+    #                         int(hunk.target_start))
 
 if __name__ == "__main__":
     main()
---
+++ scripts/get_comments_pull_request.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+def get_comments_pull_request(github_token: str, 
+                              github_repository: str, 
+                              pull_request_number: int):
+    url = f'https://api.github.com/repos/{github_repository}/pulls/{pull_request_number}/comments'
+    headers = {
+        "Accept": "application/json",
+        "authorization": f"Bearer {github_token}"
+    }
+    return requests.get(url=url, headers=headers)
+
+def get_list_comments_pull_request(github_token: str,
+                                   github_repository: str, 
+                                   pull_request_number: int):
+    
+    response = get_comments_pull_request(github_token, 
+                                         github_repository, 
+                                         pull_request_number)
+    if response.status_code == 200:
+        comments = response.json()
+        for comment in comments:
+            print(comment['id'], comment['body'])
+    else:
+        print('Falha ao obter comentários:', response.content)
