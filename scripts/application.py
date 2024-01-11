a39205dc0ffc04d5ac850c03eb33966023d747b0
RANYELL HENRIQUE DOS SANTOS <ranyellhenrique@gmail.com>
2024-01-11 02:04:51 -0300

    fix: add bard

--- scripts/application.py
+++ scripts/application.py
@@ -2,6 +2,7 @@ import os
 from get_diff_github import get_diff_by_path
 from post_comments_pull_request import post_comment_by_hunk
 from get_comments_pull_request import get_list_comments_pull_request
+from code_review_bard import code_review_by_hunk
 
 
 def main():
@@ -15,15 +16,16 @@ def main():
 
     for file in diff_files:
         for hunk in file:
-            # code_review_response = code_review_by_hunk(hunk)
-            post_comment_by_hunk(github_token, 
-                             github_repository, 
-                             pull_request_number, 
-                             git_commit_hash, 
-                             "code_review_response", 
-                             file.path, 
-                             int(hunk.target_start) + 1,
-                             list_comments)
+            code_review_response = code_review_by_hunk(hunk)
+            if code_review_response != None:
+                post_comment_by_hunk(github_token, 
+                                github_repository, 
+                                pull_request_number, 
+                                git_commit_hash, 
+                                code_review_response, 
+                                file.path, 
+                                int(hunk.target_start) + 1,
+                                list_comments)
 
 if __name__ == "__main__":
     main()
---
+++ scripts/code_review_bard.py
@@ -0,0 +1,8 @@
+from bardapi import Bard
+
+def code_review_by_hunk(hunk):
+    input_text = 'Poderia fazer o code review do diff do pull request a seguir? \n' + hunk
+    bard_output = Bard().get_answer(input_text)
+    if bard_output.status_code == 200:
+        return bard_output['content']
+    return None
